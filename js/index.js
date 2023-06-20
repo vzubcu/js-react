e71075fb73d2dd6d51f3eee16721769316d4ced9
Victor <zubcuvictor@gmail.com>
2023-06-20 13:02:58 +0300

    first

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const carBodyWidth = 5000,
+      carBodyHeight = 4000;
+
+// console.log("Ширина кузова автомобиля:" + a + ', длина ' + b);
+
+
+// ['yuru', 'agad', 'gjasd'],map(a =>)
+
+//request
+
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+let COLOR_RED = '#F00';
+
+const _apiBase = 'https//gateway..marvel.com:443/api';
+
+//api key
+const _apiKey = 'apikey=dsasdj238sf4skgha';
+
+
+// console.log(4/0);
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+
+// console.log(und);
+
+
+const obj = {
+    name: "John",
+    age: 24,
+    isMarried: false
+}
+
+// console.log(obj.name)
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp', {}, []];
+console.log(arr[1]);
+
+
