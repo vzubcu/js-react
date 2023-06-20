b35c68274f631e91b8053a35dde975b4bb302a30
Victor <zubcuvictor@gmail.com>
2023-06-20 13:03:23 +0300

    second

--- js/script.js
+++ js/script.js
@@ -3,15 +3,6 @@
 let incr = 10,
     decr = 10;
 
-// ++incr;
-// --decr;
-
-// console.log(++incr);
-// console.log(--decr);
-
-
-// console.log(5 % 2);
-
 console.log(2 + 2 * 2 !== 8);
 
 const isChecked = false,
