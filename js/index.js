975c6376bdf5ef12610a9a348ad9d92e6d6c6cc4
Victor <zubcuvictor@gmail.com>
2023-06-21 06:47:03 +0300

    Логические операторы

--- js/index.js
+++ js/index.js
@@ -1,33 +1,73 @@
-"use strict";
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
 
-// if (4 == 9) {
-//     console.log("OK!");
+// console.log(hamburger === 3 && cola && fries)
+
+// console.log(1 && 0)
+// console.log(1 && 5)
+// console.log(null && 5)
+// console.log(0 && 'asdgwerwer')
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if( hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log("esti");
 // } else {
-//     console.log("Error");
+//     console.log("netu");
 // }
 
-const num = 50;
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
 
-// if(num < 49) {
-//     console.log('Error');
-// } else{
-//     console.log('Ok!');
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+// console.log(johnReport || alexReport || samReport || mariaReport)
+
+// console.log(!0)
+
+
+// console.log(NaN || 2 || undefined)
+
+// console.log( NaN && 2 && undefined)
+
+// console.log( 1 && 2 && 3)
+
+// console.log( !1 && 2 || !3)
+
+// console.log( 25 || null && !3 )
+
+// console.log (NaN || null || !3 || undefined || 5)
+
+// console.log (NaN || null && !3 && undefined || 5)
+
+// console.log (5 === 5 && 3 > 1 || 5)
+
+// const hamburger = 3,
+//       fries = 3,
+//       cola = 0,
+//       nuggets = 2;
+      
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//     console.log('Done')
 // }
 
-(num < 49) ? console.log('Error') : console.log('Ok!');
+// let hamburger;
+// const fries = NaN,
+//       cola = 0,
+//       nuggets = 2;
 
-switch (num) {
-    case 49: 
-        console.log('Error');
-        break;
-    case 100:
-        console.log('Error');
-        break
-    case 50:
-        console.log('Good');
-        break;
-    default:
-        console.log("Another time");
-        break;
+// if (hamburger || cola || fries === 3 || nuggets) {
+//     console.log('Done')
+// }
 
+let hamburger;
+const fries = NaN,
+      cola = 0,
+      nuggets = 2;
+
+if(hamburger && cola || fries === 3 && nuggets) {
+    consosle.log('Done')
 }
