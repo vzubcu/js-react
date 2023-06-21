f3782af7c689ae1bc87472d67b8299b1703d2a7c
Victor <zubcuvictor@gmail.com>
2023-06-21 07:58:28 +0300

    Test 1

--- js/script.js
+++ js/script.js
@@ -41,13 +41,96 @@
 // console.log(result);
 
 
-first: for (let i = 0; i < 3; i++) {
-    console.log(`First level: ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`);
-        for (let k = 0; k < 5; k++) {
-            if (k === 2) continue first;
-            console.log(`Third level: ${k}`);
-        }
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 5; k++) {
+//             if (k === 2) continue first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
+
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// let i = 2
+// while (i <= 16) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+// Место для первой задачи
+function firstTask() {
+    // Пишем решение вот тут
+    for (let i = 5; i < 11; i++) {
+        console.log(i);
     }
+    
+}
+
+// Место для второй задачи
+function secondTask() {
+    // Пишем решение вот тут
+    for (let i = 0; i < 7; i++ ) {
+        console.log(20 - +i)
+    }
+    
+}
+
+// Место для третьей задачи
+function thirdTask() {
+    // Пишем решение вот тут
+    for (let i = 1; i <= 5; i++) {
+        console.log(i * 2);
+    }
+    
+}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+    // Пишем решение вот тут
+    
+    let i = 2
+    while (i <= 16) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        }
+        i++;
+    }
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    // Пишем решение вот тут
+        for (let i = 5; i <= 10; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+    
+    // Не трогаем
+    return arrayOfNumbers;
 }
