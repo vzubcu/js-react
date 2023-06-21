ab450f2d5efd730c0605bc42421895acd26c28e0
Victor <zubcuvictor@gmail.com>
2023-06-21 14:43:33 +0300

    tests

--- js/script.js
+++ js/script.js
@@ -134,3 +134,53 @@ function fifthTask() {
     // Не трогаем
     return arrayOfNumbers;
 }
+
+
+
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    // Пишем решение вот тут
+    for (i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+    
+    // Не трогаем
+    return result;
+}
+
+console.log(firstTask())
+
+
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    // Пишем решение вот тут
+    for (i = 0; i < data.length; i++) {
+        if(typeof(data[i]) == "number") {
+            data[i] = data[i] * 2;
+        } else {
+            data[i] = data[i] + " - done";
+        }
+        
+    }
+    
+    // Не трогаем
+    return data;
+}
+
+console.log(secondTask())
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+
+// Пишем решение вот тут
+for (i = 1; i <= data.length; i++) {
+    // console.log(data.length - +i)
+    console.log(data[+data.length - i])
+    result[i] = data[data.length - i]
+}
