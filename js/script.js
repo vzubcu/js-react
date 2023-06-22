486b9b770405f4b61c9ff8c522ffe064cafee2e9
Victor <zubcuvictor@gmail.com>
2023-06-22 11:34:02 +0300

    Functions

--- js/script.js
+++ js/script.js
@@ -1,51 +1,42 @@
-/* Задание на урок:
+"use strict";
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+let num = 20;
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-str.length - и получить её длину)
+//Function declaration
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
 
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+showFirstMessage("Hello");
+console.log(num);
 
-4) Потренироваться и переписать цикл еще двумя способами*/
+function calc(a, b) {
+    return (a + b);
+}
 
-'use strict';
+console.log(calc(5, 9));
+console.log(calc(5, 3));
+console.log(calc(4, 9));
+console.log(calc(6, 9));
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+function ret() {
+    let num = 50;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+    return num;
+}
+
+const aNotherNum = ret();
+console.log(aNotherNum);
+
+
+// Function Expression
+const logger = function () {
+    console.log("Hello");
 };
+logger();
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-    
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
+//Arrows functions
 
-if (personalMovieDB.count < 10) {
-    console.log('Просмотренно довольно мало фильмов');
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-    console.log('Вы класический эритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log("произишла ошибвка")
-}
-
-console.log(personalMovieDB);
+const calc = (a, b) => a + b;
