64142646d7ca6c84da06b377157df2d15d220f93
Victor <zubcuvictor@gmail.com>
2023-07-24 15:44:43 +0300

    Objects

--- js/index.js
+++ js/index.js
@@ -12,17 +12,17 @@
 
 // start();
 
-// const personalMovieDb = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: {},
-//     privat: false
-// };
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
 
 // function rememberMyFIlms() {
 //     for (let i = 0; i < 2; i++) {
-//         const a = prompt('Один из последник просмотренных фильмов?', ''),
+//         const a = prompt('Один из последник просмотренных фильмов?', '').trim(),
 //             b = prompt('На сколько оцените его?', '');
 
 //         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
@@ -69,15 +69,15 @@
 // writeYourGenres();
 
 // Место для первой задачи
-function calculateVolumeAndArea(latura) {
-    if(typeof(latura) == 'number' && latura > 0 && latura != '' && Number.isInteger(latura)){ 
-        let aria = latura * latura * latura,
-            suprafata = 6 * latura * latura;
-        return  `Объем куба: ${aria}, площадь всей поверхности: ${suprafata}`;
-    } else {
-        return "При вычислении произошла ошибка";
-    }
-}
+// function calculateVolumeAndArea(latura) {
+//     if(typeof(latura) == 'number' && latura > 0 && latura != '' && Number.isInteger(latura)){ 
+//         let aria = latura * latura * latura,
+//             suprafata = 6 * latura * latura;
+//         return  `Объем куба: ${aria}, площадь всей поверхности: ${suprafata}`;
+//     } else {
+//         return "При вычислении произошла ошибка";
+//     }
+// }
 
 // console.log(calculateVolumeAndArea(5));
 // console.log(calculateVolumeAndArea(15));
@@ -86,17 +86,17 @@ function calculateVolumeAndArea(latura) {
 // console.log(calculateVolumeAndArea(-15));
 
 // Место для второй задачи
-function getCoupeNumber(numb) {
-    if(Number.isInteger(numb) && typeof(numb) == 'number' && numb >= 0 ){
-        if(numb != 0 && numb <= 36) {
-            return Math.ceil(numb / 4);
-        } else {
-            return "Таких мест в вагоне не существует";
-        }
-    } else {
-        return "Ошибка. Проверьте правильность введенного номера места";
-    }
-}
+// function getCoupeNumber(numb) {
+//     if(Number.isInteger(numb) && typeof(numb) == 'number' && numb >= 0 ){
+//         if(numb != 0 && numb <= 36) {
+//             return Math.ceil(numb / 4);
+//         } else {
+//             return "Таких мест в вагоне не существует";
+//         }
+//     } else {
+//         return "Ошибка. Проверьте правильность введенного номера места";
+//     }
+// }
 
 // console.log(getCoupeNumber(33))
 // console.log(getCoupeNumber(7))
@@ -108,34 +108,34 @@ function getCoupeNumber(numb) {
 
 
 // Место для первой задачи
-function getTimeFromMinutes(minutes) {
-    if(Number.isInteger(minutes) && typeof(minutes) == 'number' && minutes >= 0 ){
-        let hour = parseInt(minutes / 60),
-            remain_minutes = minutes - (hour * 60),
-            hour_text;
-        switch(hour) {
-            case 0:
-                hour_text = "часов";
-                break;
-            case 1:
-                hour_text = "час";
-                break;
-            case 2:
-                hour_text = "часа";
-                break;
-            case 3:
-                hour_text = "часа";
-                break;
-            case 4:
-                hour_text = "часа";
-                break;
-            default:
-                hour_text = "часов";
-        }
-        return `Это ${hour} ${hour_text} и ${remain_minutes} минут`;
-    }
-    return "Ошибка, проверьте данные";
-}
+// function getTimeFromMinutes(minutes) {
+//     if(Number.isInteger(minutes) && typeof(minutes) == 'number' && minutes >= 0 ){
+//         let hour = parseInt(minutes / 60),
+//             remain_minutes = minutes - (hour * 60),
+//             hour_text;
+//         switch(hour) {
+//             case 0:
+//                 hour_text = "часов";
+//                 break;
+//             case 1:
+//                 hour_text = "час";
+//                 break;
+//             case 2:
+//                 hour_text = "часа";
+//                 break;
+//             case 3:
+//                 hour_text = "часа";
+//                 break;
+//             case 4:
+//                 hour_text = "часа";
+//                 break;
+//             default:
+//                 hour_text = "часов";
+//         }
+//         return `Это ${hour} ${hour_text} и ${remain_minutes} минут`;
+//     }
+//     return "Ошибка, проверьте данные";
+// }
 
 // console.log(getTimeFromMinutes(150));
 // console.log(getTimeFromMinutes(50));
@@ -143,48 +143,48 @@ function getTimeFromMinutes(minutes) {
 // console.log(getTimeFromMinutes(-150));
 
 // Место для второй задачи
-function findMaxNumber(numb_1, numb_2, numb_3, numb_4) {
-    if(typeof(numb_1) == "number" && typeof(numb_2) == "number" && typeof(numb_3) == "number" && typeof(numb_4) == "number" ) {
-        let max_1_2 = numb_1 > numb_2 ? numb_1 : numb_2,
-            max_3_4 = numb_3 > numb_4 ? numb_3 : numb_4,
-            max_final = max_1_2 > max_3_4 ? max_1_2 : max_3_4;
-        return max_final;
+// function findMaxNumber(numb_1, numb_2, numb_3, numb_4) {
+//     if(typeof(numb_1) == "number" && typeof(numb_2) == "number" && typeof(numb_3) == "number" && typeof(numb_4) == "number" ) {
+//         let max_1_2 = numb_1 > numb_2 ? numb_1 : numb_2,
+//             max_3_4 = numb_3 > numb_4 ? numb_3 : numb_4,
+//             max_final = max_1_2 > max_3_4 ? max_1_2 : max_3_4;
+//         return max_final;
 
-    }
-    return 0;
-}
+//     }
+//     return 0;
+// }
 
 // console.log(findMaxNumber(1, 5, 6.6, 11))
 // console.log(findMaxNumber(1, 5, '6', '10'))
 
 
-function fib(num) {
-    if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
-        return "";
-    }
-    let result = '';
-    let first = 0;
-    let second = 1;
+// function fib(num) {
+//     if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+//         return "";
+//     }
+//     let result = '';
+//     let first = 0;
+//     let second = 1;
 
-    for (let i = 0; i < num; i++) {
-        if (i + 1 === num) {
-            result += `${first}`;
-            // Без пробела в конце
-        } else {
-            result += `${first} `;
-        }
+//     for (let i = 0; i < num; i++) {
+//         if (i + 1 === num) {
+//             result += `${first}`;
+//             // Без пробела в конце
+//         } else {
+//             result += `${first} `;
+//         }
 
-        let third = first + second;
-        first = second;
-        second = third;
-    }
+//         let third = first + second;
+//         first = second;
+//         second = third;
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-console.log("Fib - fibona4i")
-console.log(fib(4));
-console.log(fib(7));
-console.log(fib("7"));
-console.log(fib(1));
-console.log(fib(0));
+// console.log("Fib - fibona4i")
+// console.log(fib(4));
+// console.log(fib(7));
+// console.log(fib("7"));
+// console.log(fib(1));
+// console.log(fib(0));
--- js/script.js
+++ js/script.js
@@ -1,11 +1,74 @@
 'user strict';
-let numberOfFilms;
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let numberOfFilms;
+// function start() {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-    while(numberOfFilms = '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
+//     while(numberOfFilms = '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//     }
+// }
+
+// start();
+
+function first() {
+    // Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
 }
 
-start();
+function second() {
+    console.log(2);
+}
+
+// first();
+// second();
+
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`);
+    callback();
+}
+
+function done() {
+    
+    console.log('I finished this lesson');
+}
+
+// learnJS('JavaScript', done);
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        background: 'red'
+    },
+    makeTest: function() {
+        console.log("test");
+    }
+};
+
+// console.log(Object.keys(options))
+// options.makeTest();
+
+// const {border, bg} = options.colors;
+// console.log(border)
+
+
+// delete options.name;
+// console.log(options)
+// console.log(options.colors.background)
+// let counter = 0;
+// for (let key in options) {
+//     if(typeof(options[key]) == 'object'){
+//         for (let i in options[key]){
+//             console.log(`Option ${i}, have value ${options[key][i]}`);
+//             counter++; 
+//         }
+//     } else {
+//         console.log(`Option ${key}, have value ${options[key]}`)
+//         counter++;
+//     }
+// }
+// console.log(counter);
