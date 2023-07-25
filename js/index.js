e5404dd5e906e206a902e8111c55db90cfd8d1ca
Victor <zubcuvictor@gmail.com>
2023-07-25 15:48:49 +0300

    Lesson 40

--- js/index.js
+++ js/index.js
@@ -1,71 +1,71 @@
 'use strict';
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: {},
-    privat: false,
-    start: function() {
-        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const personalMovieDB = {
+//     count: 0,
+//     movies: {},
+//     actors: {},
+//     genres: {},
+//     privat: false,
+//     start: function() {
+//         personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
         
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-        }
-    },
-    rememberMyFIlms: function() {
-        for (let i = 0; i < 2; i++) {
-            const a = prompt('Один из последник просмотренных фильмов?', '').trim(),
-                b = prompt('На сколько оцените его?', '');
+//         while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+//             personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//         }
+//     },
+//     rememberMyFIlms: function() {
+//         for (let i = 0; i < 2; i++) {
+//             const a = prompt('Один из последник просмотренных фильмов?', '').trim(),
+//                 b = prompt('На сколько оцените его?', '');
     
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('done');
-            } else {
-                console.log('error');
-                i--;
-            }
-        }
-    },
-    detectPersonalLevel: function() {
-        if (personalMovieDB.count < 10) {
-            console.log('Просмотренно довольно мало фильмов');
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log('Вы классический зритель');
-        } else if (personalMovieDB.count >= 30) {
-            console.log('Вы киноман');
-        } else {
-            console.log('Произошла ошибка');
-        }
-    },
-    showMyDB: function(hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB);
-        }
-    },
-    toggleVisibleMyDB: function() {
-        if(personalMovieDB.privat) {
-            personalMovieDB.privat = false;
-        } else {
-            personalMovieDB.privat = true;
-        }
-    },
-    writeYourGenres: function() {
-        for (let i = 1; i <= 2; i++) {
-            let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
+//             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//                 personalMovieDB.movies[a] = b;
+//                 console.log('done');
+//             } else {
+//                 console.log('error');
+//                 i--;
+//             }
+//         }
+//     },
+//     detectPersonalLevel: function() {
+//         if (personalMovieDB.count < 10) {
+//             console.log('Просмотренно довольно мало фильмов');
+//         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//             console.log('Вы классический зритель');
+//         } else if (personalMovieDB.count >= 30) {
+//             console.log('Вы киноман');
+//         } else {
+//             console.log('Произошла ошибка');
+//         }
+//     },
+//     showMyDB: function(hidden) {
+//         if (!hidden) {
+//             console.log(personalMovieDB);
+//         }
+//     },
+//     toggleVisibleMyDB: function() {
+//         if(personalMovieDB.privat) {
+//             personalMovieDB.privat = false;
+//         } else {
+//             personalMovieDB.privat = true;
+//         }
+//     },
+//     writeYourGenres: function() {
+//         for (let i = 1; i <= 2; i++) {
+//             let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
 
-            if (genres === '' || genres == null) {
-                console.log('Вы ввели некорректные данные или не ввели их вовсе');
-                i--;
-            } else {
-                personalMovieDB.genres = genres.split(', ');
-                personalMovieDB.genres.sort();
-            } 
-        }
+//             if (genres === '' || genres == null) {
+//                 console.log('Вы ввели некорректные данные или не ввели их вовсе');
+//                 i--;
+//             } else {
+//                 personalMovieDB.genres = genres.split(', ');
+//                 personalMovieDB.genres.sort();
+//             } 
+//         }
 
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Любимый жанр ${i + 1} - это ${item}`);
-        });
+//         personalMovieDB.genres.forEach((item, i) => {
+//             console.log(`Любимый жанр ${i + 1} - это ${item}`);
+//         });
 
-    }
-};
+//     }
+// };
--- js/script.js
+++ js/script.js
@@ -1,44 +1,31 @@
 'user strict';
 
-//To String
+function createCounter() {
+    let counter = 0;
 
-// !)
+    const myFunction = function() {
+        counter = counter + 1;
+        return counter;
+    }
 
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
-
-// 2) 
-console.log(typeof(null + ''));
-
-const num = 5;
-
-console.log("https://vk.com/catalog/" + num);
-
-const fronSize = 26 + 'px';
-
-//TO Number 
-//1)
-
-console.log(typeof(Number('4')));
-
-// 2)
-console.log(typeof(+'5'));
-
-// 3)
-console.log(typeof(parseInt("15px", 10)));
-
-// let answ = +prompt("Hello", "");
-
-// To boolean
-
-// 0, '', null, undefined, NaN;
-let switcher = 1;
-if (switcher) {
-    console.log('Working....');
+    return myFunction;
 }
-//2)
-console.log(typeof(Boolean('4')));
 
-//3)
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
 
-console.log(typeof(!!"4444"));
+console.log(c1, c2, c3);
+
+
+function foo(a,b) {
+    const [first] = a;
+    const {eng} = b;
+ 
+    return `${first} ${eng}`;
+}
+ 
+const result = foo(['Hello', 'Привет'], {ru: 'Мир', eng: 'World'});
+
+console.log(typeof(NaN))
