f1611c94980043936335b86e6e4bad7f7b89badd
Victor <zubcuvictor@gmail.com>
2023-07-25 10:29:21 +0300

    objects and examples

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function learnJS(lang, callback) {
 
 function done() {
     
-    console.log('I finished this lesson');
+    // console.log('I finished this lesson');
 }
 
 // learnJS('JavaScript', done);
@@ -106,8 +106,8 @@ let a = 5,
     b= a;
     b = b + 5;
 
-console.log(b);
-console.log(a);
+// console.log(b);
+// console.log(a);
 
 const obj = {
     a: 5,
@@ -139,64 +139,165 @@ const numbers = {
     }
 }
 
-const newNumbers = copy(numbers);
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-console.log(numbers);
-console.log(newNumbers);
+// const newNumbers = copy(numbers);
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+// console.log(numbers);
+// console.log(newNumbers);
 
-const add = {
-    d: 17,
-    i: 20,
-    j: {
-        m: 5,
-        p: 3
+// const add = {
+//     d: 17,
+//     i: 20,
+//     j: {
+//         m: 5,
+//         p: 3
+//     }
+// };
+
+// const clone = Object.assign({}, add);
+// clone.d = 20;
+// clone.j.m = 20;
+// console.log(add);
+// console.log(clone);
+
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'aaaaaaa';
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//     blogs = ['wordpress', 'journal', 'livejournal'],
+//     internet = [...video, ...blogs, 'vk', "facebook"];
+
+
+
+// console.log(internet)
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ["a", "b"];
+
+// const newAaray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// }
+
+// const newObjq = {...q};
+
+// console.log(newObjq)
+
+
+// const personalPlanPeter = {
+//     name: "Peter",
+//     age: "29",
+//     skills: {
+//         languages: ['ru', 'eng'],
+//         programmingLangs: {
+//             js: '20%',
+//             php: '10%'
+//         },
+//         exp: '1 month'
+//     },
+//     showAgeAndLangs: function(plan) {
+//         const {age} = plan;
+//         const {languages} = plan.skills;
+//         let str = `Мне ${age} и я владею языками:`;
+//         languages.forEach(function(lang) {
+//             str +=`${lang.toUpperCase()} `;            
+//         });
+//         return str;
+//     }
+// };
+
+// function showExperience(plan) {
+//     return plan.skills.exp;
+// }
+
+// // console.log(showExperience(personalPlanPeter));
+
+// function showProgrammingLangs(plan) {
+//     let str = '';
+//     const {programmingLangs} = plan.skills;
+//     for (let key in programmingLangs) {
+//         str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+//     }
+//     return str;
+// }
+
+// // console.log(showProgrammingLangs(personalPlanPeter));
+
+// console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+
+// const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+// function showFamily(arr) {
+//     if(arr.length > 0){
+//         return `Семья состоит из: ${arr.join(" ")}`;
+//     } else {
+//         return "Семья пуста";
+//     }
+// }
+
+// console.log(showFamily(family));
+
+// const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+// function standardizeStrings(arr) {
+//     arr.forEach(function(item){
+//         console.log(item.toLowerCase());
+//     });
+// }
+
+// standardizeStrings(favoriteCities)
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if(typeof(str) != 'string') {
+        return "Ошибка!";
     }
-};
-
-const clone = Object.assign({}, add);
-clone.d = 20;
-clone.j.m = 20;
-console.log(add);
-console.log(clone);
-
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'aaaaaaa';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'journal', 'livejournal'],
-    internet = [...video, ...blogs, 'vk', "facebook"];
-
-
-
-console.log(internet)
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
+    return str.split('').reverse().join('');
 }
 
-const num = [2, 5, 7];
+console.log(reverse(someString));
 
-log(...num);
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
-const array = ["a", "b"];
+function availableCurr(arr, missingCurr) {
+    let str = '';
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
 
-const newAaray = [...array];
+    arr.forEach(function(curr, i) {
+        if (curr !== missingCurr) {
+            str += `${curr}\n`;
+        }
+    });
 
-const q = {
-    one: 1,
-    two: 2
+    // Или
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] === missingCurr) {
+    //         continue;
+    //     }
+    //     str += `${arr[i]}\n`;
+    // }
+
+    return str;
 }
 
-const newObjq = {...q};
 
-console.log(newObjq)
-
-
+console.log(availableCurr([baseCurrencies, ...additionalCurrencies], 'CNY'));
