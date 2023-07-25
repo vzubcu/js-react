677457294cfb617a00e312a6e7be3ba5f63527cd
Victor <zubcuvictor@gmail.com>
2023-07-25 13:26:51 +0300

    lesson 38

--- js/index.js
+++ js/index.js
@@ -1,190 +1,71 @@
 'use strict';
 
-// Код возьмите из предыдущего домашнего задания
-// let numberOfFilms;
-// function start() {
-//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//     }
-// }
-
-// start();
-
-// const personalMovieDb = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: {},
-//     privat: false
-// };
-
-// function rememberMyFIlms() {
-//     for (let i = 0; i < 2; i++) {
-//         const a = prompt('Один из последник просмотренных фильмов?', '').trim(),
-//             b = prompt('На сколько оцените его?', '');
-
-//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//             personalMovieDb.movies[a] = b;
-//             console.log('done');
-//         } else {
-//             console.log('error');
-//             i--;
-//         }
-//     }
-// }
-
-// rememberMyFIlms();
-
-// function detectPersonalLevel() {
-//     if (personalMovieDb.count < 10) {
-//         console.log('Просмотренно довольно мало фильмов');
-//     } else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
-//         console.log('Вы классический зритель');
-//     } else if (personalMovieDb.count >= 30) {
-//         console.log('Вы киноман');
-//     } else {
-//         console.log('Произошла ошибка');
-//     }
-// }
-
-// detectPersonalLevel();
-// console.log(personalMovieDb);
-
-// function showMyDB(hidden) {
-//     if (!hidden) {
-//         console.log(personalMovieDb);
-//     }
-// }
-
-// showMyDB(personalMovieDb.privat);
-// function writeYourGenres() {
-//     for (let i = 1; i <= 3; i++) {
-//         const genre = +prompt(`Ваш любимый жанр под номером ${i}`);
-//         personalMovieDb.genres[i - 1] = genre;
-//     }
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
         
-// }
-// writeYourGenres();
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFIlms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последник просмотренных фильмов?', '').trim(),
+                b = prompt('На сколько оцените его?', '');
+    
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотренно довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if(personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 2; i++) {
+            let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
 
-// Место для первой задачи
-// function calculateVolumeAndArea(latura) {
-//     if(typeof(latura) == 'number' && latura > 0 && latura != '' && Number.isInteger(latura)){ 
-//         let aria = latura * latura * latura,
-//             suprafata = 6 * latura * latura;
-//         return  `Объем куба: ${aria}, площадь всей поверхности: ${suprafata}`;
-//     } else {
-//         return "При вычислении произошла ошибка";
-//     }
-// }
+            if (genres === '' || genres == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ');
+                personalMovieDB.genres.sort();
+            } 
+        }
 
-// console.log(calculateVolumeAndArea(5));
-// console.log(calculateVolumeAndArea(15));
-// console.log(calculateVolumeAndArea(15.5));
-// console.log(calculateVolumeAndArea('15'));
-// console.log(calculateVolumeAndArea(-15));
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
 
-// Место для второй задачи
-// function getCoupeNumber(numb) {
-//     if(Number.isInteger(numb) && typeof(numb) == 'number' && numb >= 0 ){
-//         if(numb != 0 && numb <= 36) {
-//             return Math.ceil(numb / 4);
-//         } else {
-//             return "Таких мест в вагоне не существует";
-//         }
-//     } else {
-//         return "Ошибка. Проверьте правильность введенного номера места";
-//     }
-// }
-
-// console.log(getCoupeNumber(33))
-// console.log(getCoupeNumber(7))
-// console.log(getCoupeNumber(300))
-// console.log(getCoupeNumber(0))
-// console.log(getCoupeNumber(7.7))
-// console.log(getCoupeNumber(-10))
-// console.log(getCoupeNumber("Hello"))
-
-
-// Место для первой задачи
-// function getTimeFromMinutes(minutes) {
-//     if(Number.isInteger(minutes) && typeof(minutes) == 'number' && minutes >= 0 ){
-//         let hour = parseInt(minutes / 60),
-//             remain_minutes = minutes - (hour * 60),
-//             hour_text;
-//         switch(hour) {
-//             case 0:
-//                 hour_text = "часов";
-//                 break;
-//             case 1:
-//                 hour_text = "час";
-//                 break;
-//             case 2:
-//                 hour_text = "часа";
-//                 break;
-//             case 3:
-//                 hour_text = "часа";
-//                 break;
-//             case 4:
-//                 hour_text = "часа";
-//                 break;
-//             default:
-//                 hour_text = "часов";
-//         }
-//         return `Это ${hour} ${hour_text} и ${remain_minutes} минут`;
-//     }
-//     return "Ошибка, проверьте данные";
-// }
-
-// console.log(getTimeFromMinutes(150));
-// console.log(getTimeFromMinutes(50));
-// console.log(getTimeFromMinutes(0));
-// console.log(getTimeFromMinutes(-150));
-
-// Место для второй задачи
-// function findMaxNumber(numb_1, numb_2, numb_3, numb_4) {
-//     if(typeof(numb_1) == "number" && typeof(numb_2) == "number" && typeof(numb_3) == "number" && typeof(numb_4) == "number" ) {
-//         let max_1_2 = numb_1 > numb_2 ? numb_1 : numb_2,
-//             max_3_4 = numb_3 > numb_4 ? numb_3 : numb_4,
-//             max_final = max_1_2 > max_3_4 ? max_1_2 : max_3_4;
-//         return max_final;
-
-//     }
-//     return 0;
-// }
-
-// console.log(findMaxNumber(1, 5, 6.6, 11))
-// console.log(findMaxNumber(1, 5, '6', '10'))
-
-
-// function fib(num) {
-//     if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
-//         return "";
-//     }
-//     let result = '';
-//     let first = 0;
-//     let second = 1;
-
-//     for (let i = 0; i < num; i++) {
-//         if (i + 1 === num) {
-//             result += `${first}`;
-//             // Без пробела в конце
-//         } else {
-//             result += `${first} `;
-//         }
-
-//         let third = first + second;
-//         first = second;
-//         second = third;
-//     }
-
-//     return result;
-// }
-
-// console.log("Fib - fibona4i")
-// console.log(fib(4));
-// console.log(fib(7));
-// console.log(fib("7"));
-// console.log(fib(1));
-// console.log(fib(0));
+    }
+};
--- js/script.js
+++ js/script.js
@@ -1,303 +1,93 @@
 'user strict';
-// let numberOfFilms;
-// function start() {
-//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//     while(numberOfFilms = '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//     }
-// }
+let str = "some";
+let strObj = new String(str);
 
-// start();
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
-function first() {
-    // Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+// console.dir([1,2,3]);
 
-function second() {
-    console.log(2);
-}
-
-// first();
-// second();
-
-function learnJS(lang, callback) {
-    console.log(`I learn: ${lang}`);
-    callback();
-}
-
-function done() {
-    
-    // console.log('I finished this lesson');
-}
-
-// learnJS('JavaScript', done);
-
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        background: 'red'
-    },
-    makeTest: function() {
-        console.log("test");
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
     }
 };
 
-// console.log(Object.keys(options))
-// options.makeTest();
-
-// const {border, bg} = options.colors;
-// console.log(border)
-
-
-// delete options.name;
-// console.log(options)
-// console.log(options.colors.background)
-// let counter = 0;
-// for (let key in options) {
-//     if(typeof(options[key]) == 'object'){
-//         for (let i in options[key]){
-//             console.log(`Option ${i}, have value ${options[key][i]}`);
-//             counter++; 
-//         }
-//     } else {
-//         console.log(`Option ${key}, have value ${options[key]}`)
-//         counter++;
-//     }
-// }
-// console.log(counter);
-
-// const arr = [1, 5, 13, 6, 8];
-// // arr[99] = 0;
-// console.log(arr.length);
-// arr.sort(compareNum)
-// // arr.pop();
-// // arr.push(10);
-// console.log(arr);
-
-// function compareNum(a, b) {
-//     return a - b;
-// }
-
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} entry array ${arr}`)
-// });
-
-// // for (let i = 0; i < arr.length; i++) {
-// //     // console.log(arr[i]);
-// // }
-
-// for (let value of arr) {
-//     console.log(value);
-// }
-
-// const str = prompt("", "");
-// const products = str.split(", ");
-// products.sort();
-// console.log(products.join('; '));
-
-let a = 5,
-    b= a;
-    b = b + 5;
-
-// console.log(b);
-// console.log(a);
-
-const obj = {
-    a: 5,
-    b: 1
-};
-
-// const copy = obj; ссылку
-
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) {
-    let objCopy = {};
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
-}
-
-// const newNumbers = copy(numbers);
-// newNumbers.a = 10;
-// newNumbers.c.x = 10;
-// console.log(numbers);
-// console.log(newNumbers);
-
-// const add = {
-//     d: 17,
-//     i: 20,
-//     j: {
-//         m: 5,
-//         p: 3
-//     }
+const john = Object.create(soldier);
+// const john = {
+//     health: 100
 // };
 
-// const clone = Object.assign({}, add);
-// clone.d = 20;
-// clone.j.m = 20;
-// console.log(add);
-// console.log(clone);
+// john.__proto__ = soldier;
 
+// Object.setPrototypeOf(john, soldier);
 
-// const oldArray = ['a', 'b', 'c'];
-// const newArray = oldArray.slice();
+// console.log(john.armor);
+// john.sayHello()
 
-// newArray[1] = 'aaaaaaa';
-// console.log(newArray);
-// console.log(oldArray);
-
-// const video = ['youtube', 'vimeo', 'rutube'],
-//     blogs = ['wordpress', 'journal', 'livejournal'],
-//     internet = [...video, ...blogs, 'vk', "facebook"];
-
-
-
-// console.log(internet)
-
-// function log(a, b, c) {
-//     console.log(a);
-//     console.log(b);
-//     console.log(c);
-// }
-
-// const num = [2, 5, 7];
-
-// log(...num);
-
-// const array = ["a", "b"];
-
-// const newAaray = [...array];
-
-// const q = {
-//     one: 1,
-//     two: 2
-// }
-
-// const newObjq = {...q};
-
-// console.log(newObjq)
-
-
-// const personalPlanPeter = {
-//     name: "Peter",
-//     age: "29",
-//     skills: {
-//         languages: ['ru', 'eng'],
-//         programmingLangs: {
-//             js: '20%',
-//             php: '10%'
-//         },
-//         exp: '1 month'
-//     },
-//     showAgeAndLangs: function(plan) {
-//         const {age} = plan;
-//         const {languages} = plan.skills;
-//         let str = `Мне ${age} и я владею языками:`;
-//         languages.forEach(function(lang) {
-//             str +=`${lang.toUpperCase()} `;            
-//         });
-//         return str;
-//     }
-// };
-
-// function showExperience(plan) {
-//     return plan.skills.exp;
-// }
-
-// // console.log(showExperience(personalPlanPeter));
-
-// function showProgrammingLangs(plan) {
-//     let str = '';
-//     const {programmingLangs} = plan.skills;
-//     for (let key in programmingLangs) {
-//         str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
-//     }
-//     return str;
-// }
-
-// // console.log(showProgrammingLangs(personalPlanPeter));
-
-// console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
-
-
-// const family = ['Peter', 'Ann', 'Alex', 'Linda'];
-
-// function showFamily(arr) {
-//     if(arr.length > 0){
-//         return `Семья состоит из: ${arr.join(" ")}`;
-//     } else {
-//         return "Семья пуста";
-//     }
-// }
-
-// console.log(showFamily(family));
-
-// const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
-
-// function standardizeStrings(arr) {
-//     arr.forEach(function(item){
-//         console.log(item.toLowerCase());
-//     });
-// }
-
-// standardizeStrings(favoriteCities)
-
-const someString = 'This is some strange string';
-
-function reverse(str) {
-    if(typeof(str) != 'string') {
-        return "Ошибка!";
-    }
-    return str.split('').reverse().join('');
-}
-
-console.log(reverse(someString));
-
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-
-function availableCurr(arr, missingCurr) {
-    let str = '';
-    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
-
-    arr.forEach(function(curr, i) {
-        if (curr !== missingCurr) {
-            str += `${curr}\n`;
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
         }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+}
+
+function isBudgetEnough(data) {
+    let square = 0;
+    let volume = 0;
+
+    data.shops.forEach(shop => {
+        square += shop.width * shop.length;
     });
 
-    // Или
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i] === missingCurr) {
-    //         continue;
-    //     }
-    //     str += `${arr[i]}\n`;
-    // }
+    volume = data.height * square;
 
-    return str;
+    if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+        return 'Бюджета достаточно';
+    } else {
+        return 'Бюджета недостаточно';
+    }
 }
 
+// console.log(isBudgetEnough(shoppingMallData));
 
-console.log(availableCurr([baseCurrencies, ...additionalCurrencies], 'CNY'));
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+}
+console.log(sortStudentsByGroups(students));
